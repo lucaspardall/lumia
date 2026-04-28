2817e81cf61a8928d7bb294aa9c117749b0279f2
lucaspardall <llouback4@gmail.com>
2026-04-28 00:07:04 +0000

    fix: remove BottomNav reference

--- apps/web/src/pages/client/ClientHome.tsx
+++ apps/web/src/pages/client/ClientHome.tsx
@@ -262,10 +262,6 @@ export default function ClientHome() {
         </div>
       </div>
 
-      {/* Bottom nav */}
-      <div className="absolute bottom-0 left-0 right-0 z-10">
-        <BottomNav />
-      </div>
     </div>
   )
 }
